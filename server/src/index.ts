ded86361edf46ec8a77c4975376695b024448ef1
Alex Petroski <alexpetroski@gmail.com>
2024-05-20 09:35:12 -0400

    Added tsconfig and concurrently to build and run server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
+
+app.get('/hi', (req, res) => {
+	res.send('HELLO~~23123~!!!!');
+});
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
